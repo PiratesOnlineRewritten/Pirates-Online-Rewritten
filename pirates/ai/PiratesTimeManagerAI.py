03b117d2bc0be191ca6a37952bf762c599e9c486
AnythingTechPro <anythingtechpro@gmail.com>
2017-06-29 12:28:00 -0400

    pirates/ai: fixed function arguments for setFrameRate field

--- pirates/ai/PiratesTimeManagerAI.py
+++ pirates/ai/PiratesTimeManagerAI.py
@@ -3,3 +3,6 @@ from direct.directnotify import DirectNotifyGlobal
 
 class PiratesTimeManagerAI(TimeManagerAI):
     notify = DirectNotifyGlobal.directNotify.newCategory('PiratesTimeManagerAI')
+
+    def setFrameRate(self, fps, deviation, numAvs, numShips, locationCode, timeInLocation, timeInGame, gameOptionsCode, vendorId, deviceId, processMemory, pageFileUsage, physicalMemory, pageFaultCount, osInfo, cpuSpeed, numCpuCores, numLogicalCpus, apiName):
+        pass
