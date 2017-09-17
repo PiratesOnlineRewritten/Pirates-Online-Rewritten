47bf46421a3a55a3867def33ce66aab36da33c9f
thetestgame <jordanmax@nxt-studios.com>
2017-09-17 11:48:11 -0500

    pirates/leveleditor: Fixed misc issues with PirateerCove0

---
+++ pirates/leveleditor/worldData/PirateerWorld.py
@@ -0,0 +1,38 @@
+from pandac.PandaModules import Point3, VBase3
+objectStruct = {
+    'Objects': {
+        '1153509705.11sdnaik': {
+            'Type': 'Region',
+            'Name': 'default',
+            'Objects': {
+                '1151689243.57hreister': {
+                    'Type': 'Island',
+                    'Name': 'PirateerCove0',
+                    'File': 'pirateerCove0',
+                    'Hpr': Point3(0.0, 0.0, 0.0),
+                    'Objects': {
+                        '1157060429.828659a2': {
+                            'Type': 'LOD Sphere',
+                            'Hpr': Point3(0.0, 0.0, 0.0),
+                            'Pos': Point3(-0.0, -0.0, 580.947),
+                            'Radi': [11520, 12520, 13520],
+                            'Scale': VBase3(1.0, 1.0, 1.0)
+                        }
+                    },
+                    'Pos': Point3(-186.051, 1925.536, 0.0),
+                    'Scale': VBase3(1.0, 1.0, 1.0),
+                    'Visual': {
+                        'Model': 'models/islands/pir_m_are_isl_pvpcove'
+                    }
+                }
+            },
+            'Visual': {}
+        }
+    },
+    'Layers': {},
+    'ObjectIds': {
+        '1151689243.57hreister': '["Objects"]["1153509705.11sdnaik"]["Objects"]["1151689243.57hreister"]',
+        '1153509705.11sdnaik': '["Objects"]["1153509705.11sdnaik"]',
+        '1157060429.828659a2': '["Objects"]["1153509705.11sdnaik"]["Objects"]["1151689243.57hreister"]["Objects"]["1157060429.828659a2"]'
+    }
+}
