d50ed9c66dba924bd9419901d8ff25ba373a5d5b
thetestgame <jordanmax@nxt-studios.com>
2017-09-19 14:34:22 -0500

    pirates/creature: Removed unnessisary Distributed Seagulls code

--- pirates/creature/DistributedSeagull.py
+++
@@ -1,7 +0,0 @@
-from pirates.creature.DistributedAnimal import DistributedAnimal
-from pirates.creature.Seagull import Seagull
-
-class DistributedSeagull(DistributedAnimal):
-
-    def __init__(self, cr):
-        DistributedAnimal.__init__(self, cr, Seagull())
