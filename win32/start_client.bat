c4c29a30132808a8d560a8d5bf27a65f18fa96bf
thetestgame <jordanmax@nxt-studios.com>
2017-09-21 22:37:35 -0500

    win32: reworked startup batch slightly; Added comments

--- win32/start_client.bat
+++ win32/start_client.bat
@@ -1,10 +1,12 @@
 @echo off
 title Pirates Online Rewritten - Client
 
-echo Choose your server
+rem Server address input selection
+echo Choose your server (Default: Localhost)
 echo #1 - Localhost
-echo #2 - Dev Server
-echo #3 - Custom
+echo #2 - Production Server
+echo #3 - Dev Server
+echo #4 - Custom
 
 :selection
 
@@ -14,16 +16,20 @@ set /P INPUT=Selection:
 if %INPUT%==1 (
     set POR_GAMESERVER=127.0.0.1
 ) else if %INPUT%==2 (
-    set POR_GAMESERVER=game.dev.piratesrewritten.com
+    set POR_GAMESERVER=game.piratesrewritten.com
 ) else if %INPUT%==3 (
+    set POR_GAMESERVER=game.dev.piratesrewritten.com
+) else if %INPUT%==4 (
     set /P POR_GAMESERVER=Gameserver: 
 ) else (
-    goto selection
+    set POR_GAMESERVER=127.0.0.1
 )
 title Pirates Online Rewritten - Client (%POR_GAMESERVER%)
 
+rem set website urls
 set GAME_INGAME_MANAGE_ACCT=http://www.piratesrewritten.com/account
 
+rem PlayToken input
 set /P POR_TOKEN=Token (Default: dev): || ^
 set POR_TOKEN=dev
 
@@ -35,11 +41,12 @@ echo ====================================
 
 cd ../
 
+rem Start the game; Use ppython2 if available
 :main
 ppython2 -h >nul 2>&1 && (
-    ppython2 -m pirates.piratesbase.PiratesStart
+    ppython2 -m pirates.piratesbase.PiratesStart >> pirates-dev-log.txt
 ) || (
-    ppython -m pirates.piratesbase.PiratesStart
+    ppython -m pirates.piratesbase.PiratesStart >> pirates-dev-log.txt
 )
 pause
 goto :main
