ab55744cecc216cc8a416dc855969ae7da32fd43
thetestgame <jordanmax@nxt-studios.com>
2017-09-21 22:44:09 -0500

    win32: Added missing environment values to Windows client startup batch script

--- win32/start_client.bat
+++ win32/start_client.bat
@@ -26,27 +26,40 @@ if %INPUT%==1 (
 )
 title Pirates Online Rewritten - Client (%POR_GAMESERVER%)
 
-rem set website urls
+rem Set website urls
 set GAME_INGAME_MANAGE_ACCT=http://www.piratesrewritten.com/account
+set GAME_INGAME_UPGRADE=http://www.piratesrewritten.com/
+set GAME_INGAME_MOREINFO=http://www.piratesrewritten.com/about/
+set GAME_INGAME_NAMING=http://www.piratesrewritten.com/piratecode/
+
+rem Set Patcher urls
+set GAME_PATCHER_FILE_OPTIONS patcher.ver
 
 rem PlayToken input
 set /P POR_TOKEN=Token (Default: dev): || ^
 set POR_TOKEN=dev
 
+rem Choose correct python command to execute the game
+ppython2 -h >nul 2>&1 && (
+    set PYTHON_CMD=ppython2
+) || (
+    set PYTHON_CMD=ppython
+)
+
+rem Log file path
+set LOG_FILE=pirates-dev-log.txt
+
 echo ====================================
 echo Starting Pirates Online Rewritten...
 echo Token: %POR_TOKEN%
 echo Gameserver: %POR_GAMESERVER%
+echo PPython: %PYTHON_CMD%
 echo ====================================
 
 cd ../
 
-rem Start the game; Use ppython2 if available
+rem Start the game using the PYTHON_CMD variable
 :main
-ppython2 -h >nul 2>&1 && (
-    ppython2 -m pirates.piratesbase.PiratesStart >> pirates-dev-log.txt
-) || (
-    ppython -m pirates.piratesbase.PiratesStart >> pirates-dev-log.txt
-)
+%PYTHON_CMD% -m pirates.piratesbase.PiratesStart >> %LOG_FILE%
 pause
 goto :main
