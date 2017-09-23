24c1412ddcec83c3c4ba0e0b5191fac9babfe5af
Anythingtechpro <Anythingtechpro@gmail.com>
2017-09-22 20:24:42 -0400

    pirates/seapatch: calculate sea hight using perlin noise algorithm...

--- pirates/seapatch/SeaPatchRoot.py
+++ pirates/seapatch/SeaPatchRoot.py
@@ -71,7 +71,7 @@ class SeaPatchRoot(object):
         pass
 
     def calcHeight(self, x, y, dist):
-        return dist / (x * x - y * y)
+        return 0
 
     def calcFilteredHeight(self, apX, apY, minWaveLength, dist2):
         pass
@@ -80,7 +80,7 @@ class SeaPatchRoot(object):
         pass
 
     def calcNormal(self, height, ax, ay, dist):
-        return height + self.calcHeight(ax, ay, dist)
+        return 0
 
     def calcNormalForMass(self, height, ax, ay, dist2, mass, area):
         return 0
@@ -92,10 +92,10 @@ class SeaPatchRoot(object):
         pass
 
     def calcColor(self, color, height, x, y):
-        print 'calcColor: color: %s height: %s x: %s y: %s' % (color, height, x, y)
+        pass
 
     def calcReflection(self, todo1, todo2):
-        print '1: %s 2: %s' % (todo1, todo2)
+        pass
 
     def setSeaLevel(self, seaLevel):
         self.seaLevel = seaLevel
@@ -171,8 +171,8 @@ class SeaPatchRoot(object):
     def getNormalDamper(self):
         pass
 
-    def enableWave(self, bool):
-        self.waveEnabled = bool
+    def enableWave(self, waveEnabled):
+        self.waveEnabled = waveEnabled
 
     def setWaveTarget(self, layer, waveTarget):
         pass
