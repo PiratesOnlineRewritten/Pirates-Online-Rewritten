39d7472bc032a0fb4d820c670a503e46bbbd80c6
Caleb Marshall <anythingtechpro@gmail.com>
2017-12-16 17:14:06 -0800

    pirates/uberdog: move pirate specific inventory methods to PirateInventory

--- pirates/uberdog/DistributedInventoryAI.py
+++ pirates/uberdog/DistributedInventoryAI.py
@@ -167,50 +167,6 @@ class DistributedInventoryAI(DistributedObjectAI):
     def d_requestInventoryComplete(self):
         self.sendUpdateToAvatarId(self.ownerId, 'requestInventoryComplete', [])
 
-    def setGoldInPocket(self, quantity):
-        self.b_setStack(InventoryType.ItemTypeMoney, quantity)
-
-    def getGoldInPocket(self):
-        item = self.getStack(InventoryType.ItemTypeMoney)
-
-        if not item:
-            return 0
-
-        return item[1]
-
-    def setOverallRep(self, quantity):
-        self.b_setAccumulator(InventoryType.OverallRep, quantity)
-
-    def getOverallRep(self):
-        item = self.getAccumulator(InventoryType.OverallRep)
-
-        if not item:
-            return 0
-
-        return item[1]
-
-    def setPotionsRep(self, quantity):
-        self.b_setAccumulator(InventoryType.PotionsRep, quantity)
-
-    def getPotionsRep(self):
-        item = self.getAccumulator(InventoryType.PotionsRep)
-
-        if not item:
-            return 0
-
-        return item[1]
-
-    def setFishingRep(self, quantity):
-        self.b_setAccumulator(InventoryType.FishingRep, quantity)
-
-    def getFishingRep(self):
-        item = self.getAccumulator(InventoryType.FishingRep)
-
-        if not item:
-            return 0
-
-        return item[1]
-
     def delete(self):
         self.air.inventoryManager.removeInventory(self)
 
--- pirates/uberdog/PirateInventoryAI.py
+++ pirates/uberdog/PirateInventoryAI.py
@@ -4,5 +4,46 @@ from direct.directnotify import DirectNotifyGlobal
 class PirateInventoryAI(DistributedInventoryAI):
     notify = DirectNotifyGlobal.directNotify.newCategory('PirateInventoryAI')
 
-    def __init__(self, air):
-        DistributedInventoryAI.__init__(self, air)
+    def setGoldInPocket(self, quantity):
+        self.b_setStack(InventoryType.ItemTypeMoney, quantity)
+
+    def getGoldInPocket(self):
+        item = self.getStack(InventoryType.ItemTypeMoney)
+
+        if not item:
+            return 0
+
+        return item[1]
+
+    def setOverallRep(self, quantity):
+        self.b_setAccumulator(InventoryType.OverallRep, quantity)
+
+    def getOverallRep(self):
+        item = self.getAccumulator(InventoryType.OverallRep)
+
+        if not item:
+            return 0
+
+        return item[1]
+
+    def setPotionsRep(self, quantity):
+        self.b_setAccumulator(InventoryType.PotionsRep, quantity)
+
+    def getPotionsRep(self):
+        item = self.getAccumulator(InventoryType.PotionsRep)
+
+        if not item:
+            return 0
+
+        return item[1]
+
+    def setFishingRep(self, quantity):
+        self.b_setAccumulator(InventoryType.FishingRep, quantity)
+
+    def getFishingRep(self):
+        item = self.getAccumulator(InventoryType.FishingRep)
+
+        if not item:
+            return 0
+
+        return item[1]
