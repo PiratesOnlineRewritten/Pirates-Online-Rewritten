05030593264759784c691b825c479194fac2a37c
thetestgame <jordanmax@nxt-studios.com>
2018-01-04 21:52:03 -0600

    parse/cloud: Updated post-discord cloud  function for game and site

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -1,12 +1,11 @@
 var request = require('request');
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+Parse.Cloud.define('ping', function(req, res) {
+  res.success('pong');
 });
 
 Parse.Cloud.define('reportHacker', function(req, res) {
-
-  //Grab Discord hacker log url
+  //Grab Discord config info
   Parse.Config.get().then(function(config) {
     var config = Parse.Config.current();
     var discord_url = config.get('hacker_discord_url');
@@ -16,10 +15,20 @@ Parse.Cloud.define('reportHacker', function(req, res) {
         res.error('Failed to post to Discord. hacker_discord_url is not defined');
     }
 
+    var message = (typeof req.params.message === undefined) ? '' : req.params.message;
+
+    var config_username = config.get('discord_username');
+    config_username = (typeof config_username === undefined) ? '' : config_username;
+    var username = (typeof req.params.username === undefined) ? config_username : req.params.message;
+
+    var config_icon_url = config.get('discord_icon_url');
+    config_icon_url = (typeof config_icon_url === undefined) ? '' : config_icon_url;
+    var icon_url = req.params.icon_url
+
     //Post to discord
     request.post(
       discord_url + '/slack',
-      { json: {"text": "Hello World!", "username": "Parse", "icon_url": "https://seeklogo.com/images/P/parse-logo-2D3985615E-seeklogo.com.png"} },
+      { json: {"text": message, "username": username, "icon_url": icon_url} },
       function(error, response, body) {
         if (error) {
           res.error(body);
