a6a82c004565f164cff2b41d619d4468e13538f9
AnythingTechPro <anythingtechpro@gmail.com>
2018-05-24 08:20:22 -0400

    pirates/world: code cleanup

--- pirates/world/AreaBuilderBaseAI.py
+++ pirates/world/AreaBuilderBaseAI.py
@@ -20,22 +20,17 @@ class AreaBuilderBaseAI(DirectObject):
             newObj = self.__createIsland(objectData, parent, parentUid, objKey, dynamic)
         else:
             if not parent or not hasattr(parent, 'builder'):
-                areaParent = self.air.worldCreator.world.uidMgr.justGetMeMeObject(parentUid)
+                parent = self.air.worldCreator.world.uidMgr.justGetMeMeObject(parentUid)
+                
+                if not parent:
+                    return
 
-                if not areaParent:
-                    return newObj
-            else:
-                areaParent = parent
-
-            newObj = areaParent.builder.createObject(objType, objectData, parent, parentUid, objKey, dynamic)
+            newObj = parent.builder.createObject(objType, objectData, parent,
+                parentUid, objKey, dynamic)
 
         return newObj
 
     def parentObjectToCell(self, object, zoneId=None):
-        if not object:
-            self.notify.warning('Failed to parent to cell for non-existant object!')
-            return
-
         if zoneId is None:
             zoneId = self.parent.getZoneFromXYZ(object.getPos())
 
@@ -51,7 +46,6 @@ class AreaBuilderBaseAI(DirectObject):
         return self.air.worldCreator.getObjectParentUid(objKey) != parentUid
 
     def setObjectTruePosHpr(self, object, objKey, parentUid, objectData):
-
         objectPos = objectData.get('Pos', Point3(0, 0, 0))
         objectHpr = objectData.get('Hpr', Point3(0, 0, 0))
 
@@ -93,8 +87,8 @@ class AreaBuilderBaseAI(DirectObject):
 
             if not 'GridPos' in objectData:
                 parentObject.setPos(parentData.get('Pos', Point3(0, 0, 0)))
+            
             parentObject.setHpr(parentData.get('Hpr', Point3(0, 0, 0)))
-
             objectPos = objectData.get('GridPos', objectData.get('Pos', Point3(0, 0, 0)))
             return objectPos, parentObject
 
@@ -126,11 +120,12 @@ class AreaBuilderBaseAI(DirectObject):
 
     def addObject(self, object, uniqueId=None):
         if not object:
-            self.notify.warning('Cannot add an invalid object!')
             return
 
         if object.doId in self.objectList:
-            self.notify.warning('Cannot add an already existing object %d!' % object.doId)
+            self.notify.warning('Cannot add an already existing object %d!' % (
+                object.doId))
+            
             return
 
         self.parent.uidMgr.addUid(uniqueId or object.getUniqueId(), object.doId)
@@ -138,11 +133,12 @@ class AreaBuilderBaseAI(DirectObject):
 
     def removeObject(self, object, uniqueId=None):
         if not object:
-            self.notify.warning('Cannot remove an invalid object!')
             return
 
         if object.doId not in self.objectList:
-            self.notify.warning('Cannot remove a non-existant object %d!' % object.doId)
+            self.notify.warning('Cannot remove a non-existant object %d!' % (
+                object.doId))
+            
             return
 
         self.parent.uidMgr.removeUid(uniqueId or object.getUniqueId())
@@ -159,16 +155,11 @@ class AreaBuilderBaseAI(DirectObject):
         object = self.objectList.get(doId)
 
         if not object:
-            self.notify.warning('Cannot delete an invalid object!')
             return
 
         object.requestDelete()
         self.removeObject(object)
 
     def broadcastObjectPosition(self, object):
-        if not object:
-            self.notify.warning('Failed to broadcast position for non-existant object!')
-            return
-
         object.d_setPos(*object.getPos())
         object.d_setHpr(*object.getHpr())
